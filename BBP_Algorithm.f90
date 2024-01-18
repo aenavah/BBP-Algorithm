00d31b143a8063272b7e452f4fc9e43e171a9714
Alexandra Nava <aenava@ucsc.edu>
2024-01-17 19:13:53 -0800

    m

---
+++ BBP_Algorithm.f90
@@ -0,0 +1,36 @@
+PROGRAM BBP_Algorithm
+implicit none
+
+integer :: n = 0
+real :: threshold, pi_approx = 0.0,  pi_true = acos(-1.d0)
+
+call threshold_crit(.000010, pi_approx, n)
+
+CONTAINS
+
+  function next_iteration(n) result(iteration)
+    integer, intent(in) :: n 
+    real :: iteration
+    iteration = (16.0 ** (-n))*((4.0/((8.0*n)+1.0)) - 2.0/((8.0*n)+4.0) - 1.0/((8.0*n)+5.0) - 1.0/((8.0*n)+6.0))
+  end function next_iteration
+
+  subroutine threshold_crit (threshold, pi_approx, n)
+    real, intent(in) :: threshold
+    real, intent(in out) :: pi_approx  
+    integer, intent(in out) :: n
+    real :: diff, current_iteration
+
+    diff = 1.0
+    !if error between approx pi and actual pi is greater than defined threshold
+    do while (diff > threshold) 
+      diff = abs(pi_approx - pi_true)
+      current_iteration = next_iteration(n)
+      pi_approx = pi_approx + current_iteration
+      n  = n + 1 
+    end do 
+    
+  write(*,*) "n: ", n
+  write(*,*) "Error: ", diff  
+  end subroutine threshold_crit 
+
+end PROGRAM BBP_Algorithm
---
+++ Makefile
@@ -0,0 +1,15 @@
+FC = gfortran
+FFLAGS = -g -Wall -Wextra -Wimplicit-interface -fmax-errors=5
+
+OBJ = BBP_Algorithm.o
+
+BBP_Algorithm.ex: $(OBJ)
+	$(FC) $(FFLAGS) -o $@ $^
+
+%.o: %.f90
+	$(FC) $(FFLAGS) -c $<
+
+.PHONY: clean
+
+clean:
+	rm -f BBP_Algorithm.ex *.o *.mod *~
